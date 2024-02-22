45779d89e8654454c57ebbbd901af2550fbf2274
dawayozerdema <155144774+dawayozerdema@users.noreply.github.com>
2024-02-22 14:17:35 +0600

    Add files via upload

---
+++ src/data/constants.js
@@ -0,0 +1,383 @@
+export const Bio = {
+  name: "Dawa Yoezer Dema",
+  roles: [
+    "Web Developer",
+    "Coder",
+    "Graphic Designer",
+    "Digita Marketer",
+    "Free Lencer",
+  ],
+  description:
+    "I am a front-end developer who loves building user-friendly websites.What drives me is the potential of Web Development.I am excited about the possibilities that each new projects brings from transforming ideas into functional code to witnessing users benefit from the projects i had created",
+  github: "https://github.com/dawayozerdema",
+  resume:
+    "https://drive.google.com/file/d/1mUnY6s8mafnLbawk2t8CrxbFatPdZxVl/view?usp=sharing",
+  linkedin: "https://www.linkedin.com",
+  twitter: "https://twitter.com",
+  insta: "https://www.instagram.com/",
+  facebook: "https://www.facebook.com",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0, 
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmUteCiqcS8RIz2imdsHVrYagTeZcBZ29wM20cdkQYIxlGhMWyamKXKU8uq-Dr_md-u1s&usqp=CAU", 
+    role: "HTML", 
+    company: "Tim Berner's ", 
+    date: "1993", 
+    desc: "The first version of HTML was written by Tim Berners-Lee in 1993. Since then, there have been many different versions of HTML. The most widely used version throughout the 2000's was HTML 4.01, which became an official standard in December 1999. Another version, XHTML, was a rewrite of HTML as an XML language.", 
+    skills: [ 
+      "ReactJS", 
+       
+      "NodeJs", 
+       
+      "HTML", 
+      "CSS", 
+      "JavaScript", 
+    ], 
+    
+  }, 
+  { 
+    id: 1, 
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQX2omepSVKl3WEyoPTF7l6TO8zea8PZa0ZcA&usqp=CAU", 
+    role: "CSS", 
+    company: "Dsp-learnio", 
+    date: "1994", 
+    desc: "1994- HÃ¥kon Wium Lie proposed the idea of CSS. 1996- The first version of CSS was invented. 1998- CSS 2 was released and work on CSS 3 began. CSS 3 was very different from the other versions, fot instead of being a single monolithic specification, it was published as a set of separate documents known as modules.", 
+    skills: [ 
+       
+       
+      "JavaScript", 
+      "TypeScript", 
+      "Node Js", 
+       
+    ], 
+     
+  }, 
+  { 
+    id: 2, 
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuyE-EWvtxtsNXh1_PFCAUQnQh2LxGIWAaKSDjOqKhMzb0fyXqkUmSE4SPNXXhjjuG1zc&usqp=CAU", 
+    role: "Java script", 
+    company: "Dsp-learnio", 
+    date: "1995", 
+    desc: "JavaScript was invented by Brendan Eich in 1995. It was developed for Netscape 2, and became the ECMA-262 standard in 1997. After Netscape handed JavaScript over to ECMA, the Mozilla foundation continued to develop JavaScript for the Firefox browser.", 
+    skills: [ 
+      
+      "Java", 
+      "Node Js", 
+      "Figma", 
+    ], 
+     
+  }, 
+  { 
+    id: 3, 
+    img: "https://media.licdn.com/dms/image/D5612AQGBBk9xRbmOeg/article-cover_image-shrink_720_1280/0/1677133669608?e=2147483647&v=beta&t=7x-eYUHglvFueqSLVKu_J9ctKuV5GhWZzOmxoRmTdbE", 
+    role: "React", 
+    company: "Dsp-learnio", 
+    date: "May 31 - 2022", 
+    desc: "May 31, 2022 — Jordan Walke created FaxJS, the early prototype of React – shipped a search element on Facebook. 2012 – Something new had started at Facebook.", 
+    skills: [ 
+      "ReactJS", 
+      "Redux", 
+      "NodeJs", 
+     "HTML", 
+      "CSS", 
+      "JavaScript", 
+      "MongoDB", 
+    ], 
+     
+  }, 
+  { 
+    id: 4, 
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/gdsc.jpeg?alt=media&token=c162329c-efaa-4be8-a173-8d3f4c48ea70", 
+    role: "Wordpress Developer", 
+    company: "Lancercode", 
+    date: "Nov2021 - Present", 
+    desc: "As an Android developer at the Google Developers Student Club (GDCS), I have had the opportunity to work on exciting projects and collaborate with talented developers who share my passion for technology. Through my involvement with GDCS, I have also had the opportunity to host and participate in numerous events, including hackathons, study jams, and workshops.", 
+    skills: [ 
+      "Leadership", 
+      "Mobile Application Development", 
+      "Kotlin", 
+      "XML", 
+      "Figma", 
+    ], 
+  },
+];
+
+export const education = [
+  {
+    id: 0, 
+    school: "Yongphula Training Center(Dsp)", 
+    date: "August 7th 2023 - September 7th 2023", 
+    grade: "A++", 
+    desc: "I am currently pursue  Ict  at Tashigang, Yongphula , Bhutan. ICT, or information and communications technology (or technologies), is the infrastructure and components that enable modern computing. Among the goals of IC technologies, tools and systems is to improve the way humans create, process and share data or information with each other. Another is to help them improve their abilities in numerous areas, including business; education; medicine; real-world problem-solving; and even leisure activities related to sports, music, and movies.", 
+    degree: "Learner's in, Excel and Ms Word",},
+    { 
+      id: 1, 
+      school: "Yonphula Training Center(Dsp)", 
+      date: "18th September 2023 -  18th November 2023", 
+      grade: "A++", 
+      desc: "I completed Cooding at Younphula Dsp center  learning basic of javascript, Html, Css and some website design as the project", 
+      degree: "Web site Design, ", 
+    }, 
+    { 
+      id: 2, 
+      school: "Gyelposhing Training Center(Dsp)", 
+      date: "4th Dec 2023 - 4th April 2024", 
+      grade: "A++", 
+      desc: "I completed my web development education at Gyelposhing(DSP),Bhutan, where I studied about Html, css, Javascript, React ", 
+      degree: "Advance in web site design ",
+  },
+];
+
+export const projects = [
+  {
+    id: 11,
+    title: "DecisionHub",
+    date: "Jan 2024 - Dec 2023",
+    description:
+      "A Rule Builder application “Decision Hub” that empowers Business Analysts to create, save, and visualize decision strategies. Provide a no-code rule writing experience and visual representation to test these rules in real-time and observe the calculations at each step.",
+    image:
+      "https://github.com/rishavchanda/DecisionHub/raw/master/assets/testRule.jpg",
+    tags: [
+      "React Js",
+      "PostgressSQL",
+      "Node Js",
+      "Express Js",
+      "Redux",
+      "React Flow",
+    ],
+    category: "web app",
+   
+  },
+  {
+    id: 9,
+    title: "Trackify",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. \nAdmin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials:	#Email: testemployee@gmail.com	#Password- 123@Testemployee",
+    image:
+      "https://user-images.githubusercontent.com/64485885/255202416-e1f89b04-2788-45b0-abc2-9dec616669e2.png",
+    tags: [
+      "Docker",
+      "AWS",
+      "DuckDNS",
+      "Eslint",
+      "Husky",
+      "CI/CD",
+      "React Js",
+      "MongoDb",
+      "Node Js",
+      "Express Js",
+      "Redux",
+    ],
+    category: "web app",
+   
+  },
+  {
+    id: 0,
+    title: "Podstream",
+    date: "Apr 2023 - May 2023",
+    description:
+      "Developed a full-stack web application that allows users to search for, play, and pause their favorite podcasts on demand and create podcasts. Implemented user authentication using Google Auth and Jwt Auth, made responsive user interface with React JS that provides users with a seamless experience across all devices. Practiced agile methodologies to optimize team efficiency and communication.",
+    image:
+      "https://user-images.githubusercontent.com/64485885/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png",
+    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
+    category: "web app",
+    
+  },
+  {
+    id: 1,
+    title: "Vexa",
+    date: "Oct 2022 - Jan 2023",
+    description:
+      "Designed and developed the Vexa project, a project management app that helps users and teams stay organized and on track. Implemented key features such as task tracking, team collaboration, and time tracking to improve productivity and project outcomes and also invite team/project members. The plan is to add a community of users where they can find developers and other team members and chat system implementation.",
+    image:
+      "https://user-images.githubusercontent.com/64485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png",
+    tags: [
+      "React Js",
+      "MongoDb",
+      "Node Js",
+      "Express Js",
+      "Redux",
+      "NodeMailer",
+    ],
+    category: "web app",
+  },
+  {
+    id: 2,
+    title: "Brain Tumor Detection",
+    date: "Jan 2023 - Mar 2023",
+    description:
+      "Preprocessed and augmented the dataset to improve model accuracy, trained the model, created API using model and Python, and used React web app for the project's front end. Achievements: Achieved an accuracy of 99.2% to accurately detect brain tumors from medical images.",
+    image:
+      "https://github.com/rishavchanda/Brain-Tumor-Detection/raw/main/Readme_resource/Image2.png",
+    tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
+    category: "machine learning",
+  
+    
+  },
+  {
+    id: 3,
+    title: "Buckoid",
+    date: "Dec 2021 - Apr 2022",
+    description:
+      "App Is Currently In Playstore 100+ Downloads. This Project proposes an “Expense Tracking App”. Keep track of your personal expenses and compare them to your monthly income with the budget planner. It has Google Drive Cloud API for Backup of User Room Database. Made with Kotlin in MVVM Architecture & Live Data.",
+    image:
+      "https://camo.githubusercontent.com/3ad28aa710d18525f1fc87de056ed53c706d09979589bfd5a773df36653bad38/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f6c6f67696e2d65613565322e61707073706f742e636f6d2f6f2f4255434b4f49442532302831292e706e673f616c743d6d6564696126746f6b656e3d32653735376235372d323964372d346263612d613562322d653164346538313432373435",
+    tags: ["Kotlin", "MVVM", "Room Database", "Google Drive Cloud API"],
+    category: "android app",
+   
+  },
+  {
+    id: 10,
+    title: "Job Finding App",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "A Job Finding App made with React Native, Axios. Users can search for any job coming from API and apply there.",
+    image:
+      "https://user-images.githubusercontent.com/64485885/255237090-cf798a2c-1b41-4bb7-b904-b5353a1f08e8.png",
+    tags: ["React Native", "JavaScript", "Axios"],
+    category: "android app",
+    
+  },
+  {
+    id: 4,
+    title: "Whatsapp Clone",
+    date: "Jul 2021",
+    description:
+      "A WhatsApp clone made with React JS, Firebase, and Material UI. It has Phone Authentication, Real-time Database. It has a chat room where users can chat with each other. It has a sidebar where users can see all the chat rooms and can create a new chat room. It has a login page where users can log in with their Google account.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/whatsapp-clone-rishav.appspot.com/o/Screenshot%20(151).png?alt=media&token=48391593-1ef0-4a8c-a92a-eb82bdf38e89",
+    tags: ["React Js", "Firebase", "Firestore", "Node JS"],
+    category: "web app",
+    
+  },
+  {
+    id: 5,
+    title: "Todo Web App",
+    date: "Jun 2021",
+    description:
+      " A Todo Web App made with React JS, Redux, and Material UI. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the tasks and can create a new task. It has a calendar where users can see all the tasks on a particular date. It has a search bar where users can search for a particular task.",
+    image:
+      "https://camo.githubusercontent.com/84ac6ab6f378348ef28d8184062b7e9e3511a1252ae3966eaa49e8e998f732a7/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f74253230283938292e706e673f616c743d6d6564696126746f6b656e3d33643335646366322d626666322d343730382d393031632d343232383866383332386633",
+    tags: ["React Js", "Local Storage", "AWS Auth", "Node JS"],
+    category: "web app",
+    
+  },
+  {
+    id: 6,
+    title: "Breaking Bad",
+    date: "Jun 2021",
+    description:
+      "A simple react app that shows the characters of the famous TV series Breaking Bad. It uses the Breaking Bad API to fetch the data. It also has a search bar to search for a particular character.",
+    image:
+      "https://camo.githubusercontent.com/937774368308a82419f53dd6eeb4a8675780e119636488b4e3cfe5d34859a72a/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f7425323028313534292e706e673f616c743d6d6564696126746f6b656e3d65613439383630632d303435362d343333342d616435372d336239346663303333363263",
+    tags: ["React Js", "API", "Axios", "Node JS"],
+    category: "web app",
+    
+  },
+  {
+    id: 7,
+    title: "Quiz App",
+    date: "Dec 2020 - Jan 2021",
+    description:
+      "A android quiz app made with Java and Firebase. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the quiz categories and can create a new quiz. It has a leaderboard where users can see the top 10 scorers. It has a search bar where users can search for a particular quiz.",
+    image:
+      "https://github-production-user-asset-6210df.s3.amazonaws.com/64485885/239726262-c1b061d1-d9d0-42ef-9f1c-0412d14bc4f6.gif",
+    tags: ["Java", "Android Studio", "Firebase", "Google Auth"],
+    category: "android app",
+   
+  },
+  {
+    id: 8,
+    title: "Face Recognition",
+    date: "Jan 2021",
+    description:
+      "A Face recognition python app made with OpenCV. It uses face_recognition library to detect faces. It uses the webcam to detect faces. It also has a search bar to search for a particular face.",
+    image:
+      "https://dontrepeatyourself.org/media/face-recognition-with-python-dlib-and-deep-learning_cezKZBj.png",
+    tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
+    category: "machine learning",
+    
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
